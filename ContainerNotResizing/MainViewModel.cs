dcddad41557e20feba23b7387e34c66ab381e883
DSPAUL <paul.de.smul@gmail.com>
2024-01-21 13:32:03 +0100

    initial commit

---
+++ ContainerNotResizing/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ContainerNotResizing
+{
+    public class MainViewModel
+    {
+        public MainViewModel(int size = 1000)
+        {
+            Items = new(Enumerable.Range(0, size));
+        }
+
+        public ObservableCollection<int> Items { get; set; }
+    }
+}
---
+++ ContainerNotResizing/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace ContainerNotResizing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainViewModel vm = new();
+            DataContext = vm;
+        }
+
+        private void Window_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            Random rnd = new();
+            MainViewModel mainViewModel = new(rnd.Next(1000));
+            DataContext = mainViewModel;
+        }
+    }
+}
